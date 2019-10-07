6483be31d9f60b77809deb708bbf6588e33c1a7c
root <bhuvnesh1510@gmail.com>
2019-10-07 14:51:58 +0530

    more packages are added

--- install_packages.sh
+++ install_packages.sh
@@ -4,14 +4,17 @@ sudo apt update
 sudo apt upgrade
 # install vim editor
 sudo apt install vim -y
+# install curl
+sudo apt install curl -y
 # install ssh server for ssh connection
 sudo apt install openssh-server -y
 # install pip3
 sudo apt install python3-pip -y
 # install ansible
-sudo pip3 install ansible -y
+sudo pip3 install ansible 
 # install git
 sudo apt install git -y
-
+# install hypervisor
+sudo apt install virt-manager
 # autoremove unused packages
 sudo apt auto-remove -y
