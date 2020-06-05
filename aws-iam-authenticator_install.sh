6108dfe2564495f9ef5155256f496118562e79c3
Bhuvnesh Sharma <bhuvnesh1510@gmail.com>
2020-06-05 16:27:26 +0530

    install aws iam authenticator on ubuntu 18.04

---
+++ aws-iam-authenticator_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator.sha256
+openssl sha1 -sha256 aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws-iam-authenticator help
+
