5784803dd5a5beb1522bb120745da2ee497e58af
Bhuvnesh Sharma <bhuvnesh1510@gmail.com>
2020-06-05 15:43:48 +0530

    permission added

--- terraform_install.sh
+++ terraform_install.sh
@@ -4,5 +4,5 @@ sudo apt update
 sudo apt install zip -y
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 unzip terraform_0.12.26_linux_amd64.zip
-mv terraform /usr/local/bin/terraform
+sudo mv terraform /usr/local/bin/terraform
 terraform -install-autocomplete #autocompletion for terraform
