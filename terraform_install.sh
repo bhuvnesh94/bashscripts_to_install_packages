fe426fa0af65dcefd7e5f0e564be38e4d317647e
Bhuvnesh Sharma <bhuvnesh1510@gmail.com>
2020-06-05 15:38:27 +0530

    terraform cli install on ubuntu 18.04

---
+++ terraform_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install zip -y
+wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+unzip terraform_0.12.26_linux_amd64.zip
+mv terraform /usr/local/bin/terraform
+terraform -install-autocomplete #autocompletion for terraform
